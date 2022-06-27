7a11c8ab09ffe2fd7e9a620e64ec56809cf87057
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-06-27 13:30:42 +0300

    Get img from web, read and show

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from time import sleep
+import requests
+import matplotlib.pyplot as pyplot
 import numpy as np
 
-arr = np.random.randint(-1, 101, (10, 10))
+url = "https://cdn2.poz.com/71174_RHSP-19-004.jpg_01b83f5c-df2d-41af-90e8-12f8af59feb8.jpeg"
+img_data_from_url = requests.get(url, stream=True).raw
 
-print(arr)
+img = pyplot.imread(img_data_from_url, format="jpeg")
+
+pyplot.imshow(img)
+pyplot.show(block=False)
+
+pyplot.pause()
+
+pyplot.close()

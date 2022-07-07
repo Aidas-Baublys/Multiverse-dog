e38f389745a728540d79ddf6fed93dff029958d4
Aidas Baublys <67596284+Aidas-Baublys@users.noreply.github.com>
2022-07-07 10:11:35 +0300

    Update auto launch script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ git clone https://github.com/Aidas-Baublys/Multiverse-dog.git
 cd Multiverse-dog
 py -m pip install --upgrade pipenv --user
 py -m pipenv install
-py main.py
+py -m pipenv run python main.py
